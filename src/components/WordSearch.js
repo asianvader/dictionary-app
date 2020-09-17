3012a71388168ecd0fe2434ac9d4bc56c0e20a85
asianvader <phoebe.voong@gmail.com>
2020-09-17 16:54:36 +0100

    Add api key to .env file. get request the api and return data.

--- src/components/WordSearch.js
+++ src/components/WordSearch.js
@@ -5,6 +5,23 @@ const WordSearch = () => {
     
     const searchWordHandler = (word) => {
         console.log(word)
+        const dictionaryApiUrl = `https://dictionaryapi.com/api/v3/references/sd2/json/${word.word}?key=${process.env.REACT_APP_WEBSTER_API_KEY}`;
+
+        console.log({dictionaryApiUrl})
+
+        fetch(dictionaryApiUrl, {
+            method: 'GET'
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((responseData) => {
+            console.log(responseData);
+            const wordClass = responseData[0].fl;
+            const definition = responseData[0].shortdef[0];
+            console.log({wordClass})
+            console.log({definition})
+        })
     }
     
     return (
